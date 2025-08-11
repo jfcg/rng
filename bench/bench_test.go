4e0208d723fc37b4eef95fdffaea8e2bfaf330e4
Serhat Şevki Dinçer <jfcgauss@gmail.com>
2025-08-11 20:30:02 +1000

    :clock3: consider initialization & arm/macos in benchmarks

--- bench/bench_test.go
+++ bench/bench_test.go
@@ -9,12 +9,15 @@ import (
 	"math/rand"
 	rand2 "math/rand/v2"
 	"testing"
+	"unsafe"
 
 	"github.com/jfcg/rng"
 )
 
 func BenchmarkGet(b *testing.B) {
 	var p rng.Prng
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for range b.N {
 		_ = p.Get()
 	}
@@ -22,28 +25,30 @@ func BenchmarkGet(b *testing.B) {
 
 func BenchmarkStdGet(b *testing.B) {
 	r := rand.New(rand.NewSource(int64(b.N)))
-	b.ResetTimer()
 	for range b.N {
 		_ = r.Uint64()
 	}
 }
 
 func BenchmarkPcgGet(b *testing.B) {
-	r := rand2.NewPCG(1, 2)
-	b.ResetTimer()
+	r := rand2.NewPCG(uint64(b.N), uint64(b.N+1))
 	for range b.N {
 		_ = r.Uint64()
 	}
 }
 
-var chaSeed = [32]byte{
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+var chaSeed [4]uint64
+
+func initCha(n int) *rand2.ChaCha8 {
+	chaSeed[0] = uint64(n)
+	chaSeed[1] = uint64(n + 1)
+	chaSeed[2] = uint64(n + 2)
+	chaSeed[3] = uint64(n + 3)
+	return rand2.NewChaCha8(*(*[32]byte)(unsafe.Pointer(&chaSeed)))
+}
 
 func BenchmarkChaGet(b *testing.B) {
-	r := rand2.NewChaCha8(chaSeed)
-	b.ResetTimer()
+	r := initCha(b.N)
 	for range b.N {
 		_ = r.Uint64()
 	}
@@ -51,6 +56,8 @@ func BenchmarkChaGet(b *testing.B) {
 
 func BenchmarkModn(b *testing.B) {
 	var p rng.Prng
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for i := range b.N {
 		_ = p.Modn(uint64(i + 1))
 	}
@@ -58,23 +65,20 @@ func BenchmarkModn(b *testing.B) {
 
 func BenchmarkStdModn(b *testing.B) {
 	r := rand.New(rand.NewSource(int64(b.N)))
-	b.ResetTimer()
 	for i := range b.N {
 		_ = r.Int63n(int64(i + 1))
 	}
 }
 
 func BenchmarkPcgModn(b *testing.B) {
-	r := rand2.New(rand2.NewPCG(1, 2))
-	b.ResetTimer()
+	r := rand2.New(rand2.NewPCG(uint64(b.N), uint64(b.N+1)))
 	for i := range b.N {
 		_ = r.Int64N(int64(i + 1))
 	}
 }
 
 func BenchmarkChaModn(b *testing.B) {
-	r := rand2.New(rand2.NewChaCha8(chaSeed))
-	b.ResetTimer()
+	r := rand2.New(initCha(b.N))
 	for i := range b.N {
 		_ = r.Int64N(int64(i + 1))
 	}
@@ -82,34 +86,33 @@ func BenchmarkChaModn(b *testing.B) {
 
 const permN = 100
 
+var permLs [permN]uint32
+
 func BenchmarkPerm(b *testing.B) {
 	var p rng.Prng
-	lu := make([]uint32, permN)
-	b.ResetTimer()
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for range b.N {
-		p.Permute(lu)
+		p.Permute(permLs[:])
 	}
 }
 
 func BenchmarkStdPerm(b *testing.B) {
 	r := rand.New(rand.NewSource(int64(b.N)))
-	b.ResetTimer()
 	for range b.N {
 		_ = r.Perm(permN)
 	}
 }
 
 func BenchmarkPcgPerm(b *testing.B) {
-	r := rand2.New(rand2.NewPCG(1, 2))
-	b.ResetTimer()
+	r := rand2.New(rand2.NewPCG(uint64(b.N), uint64(b.N+1)))
 	for range b.N {
 		_ = r.Perm(permN)
 	}
 }
 
 func BenchmarkChaPerm(b *testing.B) {
-	r := rand2.New(rand2.NewChaCha8(chaSeed))
-	b.ResetTimer()
+	r := rand2.New(initCha(b.N))
 	for range b.N {
 		_ = r.Perm(permN)
 	}
@@ -117,6 +120,8 @@ func BenchmarkChaPerm(b *testing.B) {
 
 func BenchmarkExp(b *testing.B) {
 	var p rng.Prng
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for range b.N {
 		_ = p.Exp()
 	}
@@ -124,22 +129,19 @@ func BenchmarkExp(b *testing.B) {
 
 func BenchmarkStdExp(b *testing.B) {
 	r := rand.New(rand.NewSource(int64(b.N)))
-	b.ResetTimer()
 	for range b.N {
 		_ = r.ExpFloat64()
 	}
 }
 
 func BenchmarkPcgExp(b *testing.B) {
-	r := rand2.New(rand2.NewPCG(1, 2))
-	b.ResetTimer()
+	r := rand2.New(rand2.NewPCG(uint64(b.N), uint64(b.N+1)))
 	for range b.N {
 		_ = r.ExpFloat64()
 	}
 }
 func BenchmarkChaExp(b *testing.B) {
-	r := rand2.New(rand2.NewChaCha8(chaSeed))
-	b.ResetTimer()
+	r := rand2.New(initCha(b.N))
 	for range b.N {
 		_ = r.ExpFloat64()
 	}
@@ -147,6 +149,8 @@ func BenchmarkChaExp(b *testing.B) {
 
 func BenchmarkOne(b *testing.B) {
 	var p rng.Prng
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for range b.N {
 		_ = p.One()
 	}
@@ -154,22 +158,19 @@ func BenchmarkOne(b *testing.B) {
 
 func BenchmarkStdOne(b *testing.B) {
 	r := rand.New(rand.NewSource(int64(b.N)))
-	b.ResetTimer()
 	for range b.N {
 		_ = r.Float64()
 	}
 }
 
 func BenchmarkPcgOne(b *testing.B) {
-	r := rand2.New(rand2.NewPCG(1, 2))
-	b.ResetTimer()
+	r := rand2.New(rand2.NewPCG(uint64(b.N), uint64(b.N+1)))
 	for range b.N {
 		_ = r.Float64()
 	}
 }
 func BenchmarkChaOne(b *testing.B) {
-	r := rand2.New(rand2.NewChaCha8(chaSeed))
-	b.ResetTimer()
+	r := rand2.New(initCha(b.N))
 	for range b.N {
 		_ = r.Float64()
 	}
@@ -177,6 +178,8 @@ func BenchmarkChaOne(b *testing.B) {
 
 func BenchmarkTwo(b *testing.B) {
 	var p rng.Prng
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for range b.N {
 		_ = p.Two()
 	}
@@ -184,59 +187,56 @@ func BenchmarkTwo(b *testing.B) {
 
 func BenchmarkNormal(b *testing.B) {
 	var p rng.Prng
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for range b.N {
-		_, _ = p.Normal()
+		_, _ = p.Normal() // two samples per call
 	}
 }
 
 func BenchmarkStdNormal(b *testing.B) {
 	r := rand.New(rand.NewSource(int64(b.N)))
-	b.ResetTimer()
 	for range b.N {
 		_ = r.NormFloat64()
 	}
 }
 
 func BenchmarkPcgNormal(b *testing.B) {
-	r := rand2.New(rand2.NewPCG(1, 2))
-	b.ResetTimer()
+	r := rand2.New(rand2.NewPCG(uint64(b.N), uint64(b.N+1)))
 	for range b.N {
 		_ = r.NormFloat64()
 	}
 }
 func BenchmarkChaNormal(b *testing.B) {
-	r := rand2.New(rand2.NewChaCha8(chaSeed))
-	b.ResetTimer()
+	r := rand2.New(initCha(b.N))
 	for range b.N {
 		_ = r.NormFloat64()
 	}
 }
 
-const readN = 500
+const readN = 1007
+
+var readLs [readN]byte
 
 func BenchmarkRead(b *testing.B) {
 	var p rng.Prng
-	buf := make([]byte, readN)
-	b.ResetTimer()
+	p.Put(uint64(b.N))
+	p.Put(uint64(b.N + 1))
 	for range b.N {
-		p.Fill(buf)
+		p.Fill(readLs[:])
 	}
 }
 
 func BenchmarkStdRead(b *testing.B) {
-	buf := make([]byte, readN)
 	r := rand.New(rand.NewSource(int64(b.N)))
-	b.ResetTimer()
 	for range b.N {
-		_, _ = r.Read(buf)
+		_, _ = r.Read(readLs[:])
 	}
 }
 
 func BenchmarkChaRead(b *testing.B) {
-	buf := make([]byte, readN)
-	r := rand2.NewChaCha8(chaSeed)
-	b.ResetTimer()
+	r := initCha(b.N)
 	for range b.N {
-		_, _ = r.Read(buf)
+		_, _ = r.Read(readLs[:])
 	}
 }
